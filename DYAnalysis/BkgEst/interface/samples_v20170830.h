26a26fde82e88208f7fb341edad361e5e5c76b3a
Emilien Chapon <emilien.chapon@cern.ch>
2017-09-19 19:37:40 +0200

    Add private DYTauTau

--- DYAnalysis/BkgEst/interface/samples_v20170830.h
+++ DYAnalysis/BkgEst/interface/samples_v20170830.h
@@ -63,12 +63,12 @@ namespace DYana_v20170830 {
    double Xsec(SampleTag s) {
       switch (s) {
          // MCFM NLO CT14 pPb
-         case DYMuMu1030:return 3.*208.*0.476661E+04; break; // MCFM only generates p(p/n) and 1 flavour
-         case DYMuMu30:  return 3.*208.*0.614412E+04; break; // MCFM only generates p(p/n) and 1 flavour
-         case DYMuMu1030_PbP:return 3.*208.*0.476661E+04; break; // MCFM only generates p(p/n) and 1 flavour
-         case DYMuMu30_PbP:  return 3.*208.*0.614412E+04; break; // MCFM only generates p(p/n) and 1 flavour
-         case DYTauTau1030:return 3.*208.*0.476661E+04; break; // MCFM only generates p(p/n) and 1 flavour
-         case DYTauTau30:  return 3.*208.*0.614412E+04; break; // MCFM only generates p(p/n) and 1 flavour
+         case DYMuMu1030:return 208.*0.476661E+04; break; // MCFM only generates p(p/n)
+         case DYMuMu30:  return 208.*0.614412E+04; break; // MCFM only generates p(p/n)
+         case DYMuMu1030_PbP:return 208.*0.476661E+04; break; // MCFM only generates p(p/n)
+         case DYMuMu30_PbP:  return 208.*0.614412E+04; break; // MCFM only generates p(p/n)
+         case DYTauTau1030:return 208.*0.476661E+04; break; // MCFM only generates p(p/n)
+         case DYTauTau30:  return 208.*0.614412E+04; break; // MCFM only generates p(p/n)
          case WW:        return 3.*3.*208.*0.678813; break; // MCFM only generates p(p/n) and 1 flavour 
          case WZ:        return 3.*(1./10.63e-2)*208.*45.9629e-3; break; // MCFM: p(p/n), W(->munu)Z(->ee) / PYTHIA: W(->X)Z(->ll)
          case ZZ:        return 3.*3.*208.*18.0587e-3; break; // MCFM only generates p(p/n) and 1 flavour
@@ -94,8 +94,8 @@ namespace DYana_v20170830 {
          case DYMuMu30:    return 1e6; break;
          case DYMuMu1030_PbP:  return 1e6; break;
          case DYMuMu30_PbP:    return 1e6; break;
-         case DYTauTau1030:return 1; break; //FIXME
-         case DYTauTau30:  return 1; break; //FIXME
+         case DYTauTau1030:return 94500; break;
+         case DYTauTau30:  return 97500; break;
          case WW:          return 100e3; break;
          case WZ:          return 97e3; break;
          case ZZ:          return 100e3; break;
@@ -138,8 +138,8 @@ namespace DYana_v20170830 {
          case DYMuMu30:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_pPb_Powheg_20170831/170831_093521/0000/ntuple_skim*root"; break;
          case DYMuMu1030_PbP:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_PbP_Powheg_20170817/170817_151934/0000/ntuple_skim*root"; break;
          case DYMuMu30_PbP:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_PbP_Powheg_20170817/170817_152017/0000/ntuple_skim*root"; break;
-         case DYTauTau1030:return "FIXME"; break;
-         case DYTauTau30:return "FIXME"; break;
+         case DYTauTau1030:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/Powheg_DY_M_10_30_tautau_embd_PbP/crab_DYtoTauTau_M-10_30_PbP_Powheg_20170919/170919_113527/0000/ntuple_skim*root"; break;
+         case DYTauTau30:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/Powheg_DY_M_30_tautau_embd_PbP/crab_DYtoTauTau_M-30_PbP_Powheg_20170919/170919_113435/0000/ntuple_skim*root"; break;
          case WW:        return "/afs/cern.ch/user/e/echapon/workspace/public/DY_pA_2016/trees_20170518/tree_WW.root"; break;
          case WZ:        return "/afs/cern.ch/user/e/echapon/workspace/public/DY_pA_2016/trees_20170518/tree_WZ.root"; break;
          case ZZ:        return "/afs/cern.ch/user/e/echapon/workspace/public/DY_pA_2016/trees_20170518/tree_ZZ.root"; break;
@@ -254,8 +254,8 @@ namespace DYana_v20170830 {
          case DYMuMu30:  return "DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_pPb_Powheg_20170831/170831_093521/0000/"; break;
          case DYMuMu1030_PbP:return "DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_PbP_Powheg_20170817/170817_151934/0000/"; break;
          case DYMuMu30_PbP:  return "DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_PbP_Powheg_20170817/170817_152017/0000/"; break;
-         case DYTauTau1030:return "FIXME"; break;
-         case DYTauTau30:return "FIXME"; break;
+         case DYTauTau1030:return "Powheg_DY_M_10_30_tautau_embd_PbP/crab_DYtoTauTau_M-10_30_PbP_Powheg_20170919/170919_113527/0000/"; break;
+         case DYTauTau30:return "Powheg_DY_M_30_tautau_embd_PbP/crab_DYtoTauTau_M-30_PbP_Powheg_20170919/170919_113435/0000/"; break;
          case WW:        return "Pythia6_WW_embd_pPb/crab_WW_DYntuple_20170518/170517_222918/0000/";
          case WZ:        return "Pythia6_WZ_embd_pPb/crab_WZ_DYntuple_20170518/170517_223145/0000/";
          case ZZ:        return "Pythia6_ZZ_embd_pPb/crab_ZZ_DYntuple_20170518/170517_223512/0000/";
