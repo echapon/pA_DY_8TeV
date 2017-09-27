a8aba994ae071fc770fc761c25a3853f51df3546
Emilien Chapon <emilien.chapon@cern.ch>
2017-09-27 19:11:59 +0200

    Fix ZZ xsec?

--- DYAnalysis/BkgEst/interface/samples_v20170830_Pyquen.h
+++ DYAnalysis/BkgEst/interface/samples_v20170830_Pyquen.h
@@ -71,7 +71,7 @@ namespace DYana_v20170830_Pyquen {
          case DYTauTau30:  return 265.810; break; // Powheg
          case WW:        return 3.*3.*208.*0.678813; break; // MCFM only generates p(p/n) and 1 flavour 
          case WZ:        return 3.*(1./10.63e-2)*208.*(0.0459629+0.036782); break; // MCFM: p(p/n), W+-(->munu)Z(->ee) / PYTHIA: W(->X)Z(->ll)
-         case ZZ:        return (((3*3.3658+20)/3.3658)^2)*208.*18.0587e-3; break; // MCFM: p(p/n), ZZ->eemumu / PYTHIA: ZZ->lll'l' (where l=e,mu,tau,nu)
+         case ZZ:        return pow((3*3.3658+20)/3.3658,2)*208.*18.0587e-3; break; // MCFM: p(p/n), ZZ->eemumu / PYTHIA: ZZ->lll'l' (where l=e,mu,tau,nu)
          case TT:        return 56.6e3; break; // according to DDE: NNLO (MCFM + Top++, NNPDF3.0+EPS09)
          // // PYTHIA
          // case WW:        return 208.*3.728; break;
@@ -91,7 +91,7 @@ namespace DYana_v20170830_Pyquen {
    int Nevts(SampleTag s) {
       switch (s) {
          case DYMuMu1030:  return 997120; break; // was 997120 but seems to contain 10-600 // 789275
-         case DYMuMu30:    return 992450; break;
+         case DYMuMu30:    return 978253; break;
          case DYMuMu1030_PbP:  return 1e6; break; // was 1e6 but contains 10-600 // 795824
          case DYMuMu30_PbP:    return 988732; break;
          case DYTauTau1030:return 94500; break;
@@ -137,7 +137,7 @@ namespace DYana_v20170830_Pyquen {
    const char* PathTuple(SampleTag s) {
       switch (s) {
          case DYMuMu1030:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-10to30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_pPb_Powheg_20170905/170905_103354/0000/ntuple_skim*root"; break;
-         case DYMuMu30:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_pPb-Bst_8p16TeV-Pyquen/crab_DYtoMuMu_M-30_pPb_Pyquen_20170919/170919_163037/0000/ntuple_skim*root"; break;
+         case DYMuMu30:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_pPb_Pyquen_20170926/170926_163343/0000/ntuple_skim_*.root"; break;
          case DYMuMu1030_PbP:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_PbP_Powheg_20170817/170817_151934/0000/ntuple_skim*root"; break;
          case DYMuMu30_PbP:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_PbP_Pyquen_20170817/170817_152152/0000/ntuple_skim*root"; break;
          case DYTauTau1030:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/Powheg_DY_M_10_30_tautau_embd_PbP/crab_DYtoTauTau_M-10_30_PbP_Powheg_20170919/170919_113527/0000/ntuple_skim*root"; break;
@@ -253,7 +253,7 @@ namespace DYana_v20170830_Pyquen {
    const char* NtupleDir(SampleTag s) {
       switch(s) {
          case DYMuMu1030:return "DYtoMuMu_M-10to30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_pPb_Powheg_20170905/170905_103354/0000/"; break;
-         case DYMuMu30:  return "DYtoMuMu_M-30_pPb-Bst_8p16TeV-Pyquen/crab_DYtoMuMu_M-30_pPb_Pyquen_20170919/170919_163037/0000/"; break;
+         case DYMuMu30:  return "DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_pPb_Pyquen_20170926/170926_163343/0000/"; break;
          case DYMuMu1030_PbP:return "DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_PbP_Powheg_20170817/170817_151934/0000/"; break;
          case DYMuMu30_PbP:  return "DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_PbP_Pyquen_20170817/170817_152152/0000/"; break;
          case DYTauTau1030:return "Powheg_DY_M_10_30_tautau_embd_PbP/crab_DYtoTauTau_M-10_30_PbP_Powheg_20170919/170919_113527/0000/"; break;
