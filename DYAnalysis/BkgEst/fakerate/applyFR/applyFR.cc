575e320b7f9f947ef52115fcc1a4174478164517
HyunchulKim <hyunchul.kim@cern.ch>
2018-01-16 10:03:53 +0100

    add phistar, etc

--- DYAnalysis/BkgEst/fakerate/applyFR/applyFR.cc
+++ DYAnalysis/BkgEst/fakerate/applyFR/applyFR.cc
@@ -66,6 +66,22 @@ void applyFR(SampleTag index) {
     TH1D* rapSameDijet1 = new TH1D("rapSameDijet1","",48,-2.4,2.4);
     TH1D* rapSameDijet2 = new TH1D("rapSameDijet2","",48,-2.4,2.4);
 
+// added
+    TH1D* ZptDijet1 = new TH1D("ZptDijet1","",ptbinnum_meas,ptbin_meas);
+    TH1D* ZptDijet2 = new TH1D("ZptDijet2","",ptbinnum_meas,ptbin_meas);
+    TH1D* ZptSameDijet1 = new TH1D("ZptSameDijet1","",ptbinnum_meas,ptbin_meas);
+    TH1D* ZptSameDijet2 = new TH1D("ZptSameDijet2","",ptbinnum_meas,ptbin_meas);
+
+    TH1D* lowMrapDijet1 = new TH1D("lowMrapDijet1","",rapbinnum_1560,rapbin_1560);
+    TH1D* lowMrapDijet2 = new TH1D("lowMrapDijet2","",rapbinnum_1560,rapbin_1560);
+    TH1D* lowMrapSameDijet1 = new TH1D("lowMrapSameDijet1","",rapbinnum_1560,rapbin_1560);
+    TH1D* lowMrapSameDijet2 = new TH1D("lowMrapSameDijet2","",rapbinnum_1560,rapbin_1560);
+
+    TH1D* ZphistarDijet1 = new TH1D("ZphistarDijet1","",phistarnum,phistarbin);
+    TH1D* ZphistarDijet2 = new TH1D("ZphistarDijet2","",phistarnum,phistarbin);
+    TH1D* ZphistarSameDijet1 = new TH1D("ZphistarSameDijet1","",phistarnum,phistarbin);
+    TH1D* ZphistarSameDijet2 = new TH1D("ZphistarSameDijet2","",phistarnum,phistarbin);
+
 
 
 
@@ -84,6 +100,24 @@ void applyFR(SampleTag index) {
     TH1D* rapSameWJets1 = new TH1D("rapSameWJets1","",48,-2.4,2.4);
     TH1D* rapSameWJets2 = new TH1D("rapSameWJets2","",48,-2.4,2.4);
 
+// added
+    TH1D* ZptWJets1 = new TH1D("ZptWJets1","",ptbinnum_meas,ptbin_meas);
+    TH1D* ZptWJets2 = new TH1D("ZptWJets2","",ptbinnum_meas,ptbin_meas);
+    TH1D* ZptSameWJets1 = new TH1D("ZptSameWJets1","",ptbinnum_meas,ptbin_meas);
+    TH1D* ZptSameWJets2 = new TH1D("ZptSameWJets2","",ptbinnum_meas,ptbin_meas);
+
+    TH1D* lowMrapWJets1 = new TH1D("lowMrapWJets1","",rapbinnum_1560,rapbin_1560);
+    TH1D* lowMrapWJets2 = new TH1D("lowMrapWJets2","",rapbinnum_1560,rapbin_1560);
+    TH1D* lowMrapSameWJets1 = new TH1D("lowMrapSameWJets1","",rapbinnum_1560,rapbin_1560);
+    TH1D* lowMrapSameWJets2 = new TH1D("lowMrapSameWJets2","",rapbinnum_1560,rapbin_1560);
+
+    TH1D* ZphistarWJets1 = new TH1D("ZphistarWJets1","",phistarnum,phistarbin);
+    TH1D* ZphistarWJets2 = new TH1D("ZphistarWJets2","",phistarnum,phistarbin);
+    TH1D* ZphistarSameWJets1 = new TH1D("ZphistarSameWJets1","",phistarnum,phistarbin);
+    TH1D* ZphistarSameWJets2 = new TH1D("ZphistarSameWJets2","",phistarnum,phistarbin);
+
+
+
     TH1D* histPass = new TH1D("histPass","",10,0,10);
     TH1D* histFail = new TH1D("histFail","",10,0,10);
 
@@ -96,7 +130,8 @@ void applyFR(SampleTag index) {
     double eta = 0;
     double wt = 1.0;
     double wtsum = 0;
-    bool leading = false;
+   double phistar = -999.0;
+	  bool leading = false;
 
     double FR1_template;
     double FR2_template;
@@ -161,6 +196,7 @@ void applyFR(SampleTag index) {
                 mass = (tempMuons->first.momentum() + tempMuons->second.momentum()).M();
                 sign = tempMuons->first.charge * tempMuons->second.charge;
                 rap  = (tempMuons->first.momentum() + tempMuons->second.momentum()).Rapidity();
+					phistar = Object::phistar(tempMuons->first.momentum(), tempMuons->second.momentum() );
 
                 if( sign < 0 ) {
                     if( mass > bins[0] && mass < bins[binnum-1]) {
@@ -171,6 +207,16 @@ void applyFR(SampleTag index) {
                         rapDijet1->Fill(rap, weight_template);
                         rapDijet2->Fill(rap, weight_ratio);
                     }
+							if( mass > 60 && mass < 120) {
+								ZptDijet1->Fill(pt, weight_template);
+								ZptDijet2->Fill(pt, weight_ratio);
+								ZphistarDijet1->Fill(phistar, weight_template);
+								ZphistarDijet2->Fill(phistar, weight_ratio);
+							}
+							if ( mass > 15 && mass < 60) {
+								lowMrapDijet1->Fill(rap, weight_template);
+								lowMrapDijet2->Fill(rap, weight_ratio);
+							}
                 }
                 else {
                     if( mass > bins[0] && mass < bins[binnum-1]) {
@@ -181,7 +227,17 @@ void applyFR(SampleTag index) {
                         rapSameDijet1->Fill(rap, weight_template);
                         rapSameDijet2->Fill(rap, weight_ratio);
                     }
-                }
+   							if( mass > 60 && mass < 120) {
+								ZptSameDijet1->Fill(pt, weight_template);
+								ZptSameDijet2->Fill(pt, weight_ratio);
+								ZphistarSameDijet1->Fill(phistar, weight_template);
+								ZphistarSameDijet2->Fill(phistar, weight_ratio);
+							}
+							if ( mass > 15 && mass < 60) {
+								lowMrapSameDijet1->Fill(rap, weight_template);
+								lowMrapSameDijet2->Fill(rap, weight_ratio);
+							}
+             }
             }
         }
         else if( failingMuons->size()==1 && passingMuons->size()==1 ) {
@@ -194,6 +250,8 @@ void applyFR(SampleTag index) {
                 mass = (tempMuons->first.momentum() + tempMuons->second.momentum()).M();
                 sign = tempMuons->first.charge * tempMuons->second.charge;
                 rap  = (tempMuons->first.momentum() + tempMuons->second.momentum()).Rapidity();
+					phistar = Object::phistar(tempMuons->first.momentum(), tempMuons->second.momentum() );
+
 
                 if( sign < 0 ) {
                     if( mass > bins[0] && mass < bins[binnum-1]) {
@@ -204,6 +262,17 @@ void applyFR(SampleTag index) {
                         rapWJets1->Fill(rap, weight_template);
                         rapWJets2->Fill(rap, weight_ratio);
                     }
+							if( mass > 60 && mass < 120) {
+								ZptWJets1->Fill(pt, weight_template);
+								ZptWJets2->Fill(pt, weight_ratio);
+								ZphistarWJets1->Fill(phistar, weight_template);
+								ZphistarWJets2->Fill(phistar, weight_ratio);
+							}
+							if ( mass > 15 && mass < 60) {
+								lowMrapWJets1->Fill(rap, weight_template);
+								lowMrapWJets2->Fill(rap, weight_ratio);
+							}
+
                 }
                 else {
                     if( mass > bins[0] && mass < bins[binnum-1]) {
@@ -214,6 +283,17 @@ void applyFR(SampleTag index) {
                         rapSameWJets1->Fill(rap, weight_template);
                         rapSameWJets2->Fill(rap, weight_ratio);
                     }
+   							if( mass > 60 && mass < 120) {
+								ZptSameWJets1->Fill(pt, weight_template);
+								ZptSameWJets2->Fill(pt, weight_ratio);
+								ZphistarSameWJets1->Fill(phistar, weight_template);
+								ZphistarSameWJets2->Fill(phistar, weight_ratio);
+							}
+							if ( mass > 15 && mass < 60) {
+								lowMrapSameWJets1->Fill(rap, weight_template);
+								lowMrapSameWJets2->Fill(rap, weight_ratio);
+							}
+
                 }
             }
         } 
