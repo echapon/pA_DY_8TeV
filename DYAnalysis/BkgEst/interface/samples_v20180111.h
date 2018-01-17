8f305a57303572c8b6f86c9e84d80a553620c1c1
Emilien Chapon <emilien.chapon@cern.ch>
2018-01-17 19:09:07 +0100

    New samples

--- DYAnalysis/BkgEst/interface/samples_v20180111.h
+++ DYAnalysis/BkgEst/interface/samples_v20180111.h
@@ -1,10 +1,10 @@
-#ifndef samples_v20180111_Pyquen_h
-#define samples_v20180111_Pyquen_h
+#ifndef samples_v20180111_h
+#define samples_v20180111_h
 
 #include <vector>
 #include "analysis.h"
 
-namespace DYana_v20180111_Pyquen {
+namespace DYana_v20180111 {
 
    // list of available processes (MC + data)
    enum SampleTag : int {
@@ -91,9 +91,9 @@ namespace DYana_v20180111_Pyquen {
    int Nevts(SampleTag s) {
       switch (s) {
          case DYMuMu1030:  return 997120; break; // was 997120 but seems to contain 10-600 // 789275
-         case DYMuMu30:    return 978253; break;
+         case DYMuMu30:    return 1e6; break;
          case DYMuMu1030_PbP:  return 1e6; break; // was 1e6 but contains 10-600 // 795824
-         case DYMuMu30_PbP:    return 988732; break;
+         case DYMuMu30_PbP:    return 1e6; break;
          case DYTauTau1030:return 464494; break;
          case DYTauTau30:  return 498444; break;
          case WW:          return 195810; break;
@@ -137,9 +137,9 @@ namespace DYana_v20180111_Pyquen {
    const char* PathTuple(SampleTag s) {
       switch (s) {
          case DYMuMu1030:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-10to30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_pPb_Powheg_20170905/170905_103354/0000/ntuple_skim*root"; break;
-         case DYMuMu30:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_pPb_Pyquen_20170926/170926_163343/0000/ntuple_skim_*.root"; break;
+         case DYMuMu30:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_pPb_Powheg_20170831/170831_093521/0000/ntuple_skim*root"; break;
          case DYMuMu1030_PbP:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_PbP_Powheg_20170817/170817_151934/0000/ntuple_skim*root"; break;
-         case DYMuMu30_PbP:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_PbP_Pyquen_20170817/170817_152152/0000/ntuple_skim*root"; break;
+         case DYMuMu30_PbP:  return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_PbP_Powheg_20170817/170817_152017/0000/ntuple_skim*root"; break;
          case DYTauTau1030:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_PowhegTauola/crab_DYtoTauTau_M-10to30_20180117_2/180117_132637/0000/ntuple_skim*root"; break;
          case DYTauTau30:return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/DYtoMuMu_M-30_PbP-EmbEPOS_8p16_PowhegTauola/crab_DYtoTauTau_M-30_20180117/180117_132803/0000/ntuple_skim*root"; break;
          case WW:        return "/eos/cms/store/group/phys_heavyions/dileptons/echapon/pA_8p16TeV/DYtuples/WW_PbP-EmbEPOS_8p16_Pythia8/crab_WW_20180117/180117_132840/0000/ntuple_skim*root"; break;
@@ -253,9 +253,9 @@ namespace DYana_v20180111_Pyquen {
    const char* NtupleDir(SampleTag s) {
       switch(s) {
          case DYMuMu1030:return "DYtoMuMu_M-10to30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_pPb_Powheg_20170905/170905_103354/0000/"; break;
-         case DYMuMu30:  return "DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_pPb_Pyquen_20170926/170926_163343/0000/"; break;
+         case DYMuMu30:  return "DYtoMuMu_M-30_pPb-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_pPb_Powheg_20170831/170831_093521/0000/"; break;
          case DYMuMu1030_PbP:return "DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-10to30_PbP_Powheg_20170817/170817_151934/0000/"; break;
-         case DYMuMu30_PbP:  return "DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Pyquen/crab_DYtoMuMu_M-30_PbP_Pyquen_20170817/170817_152152/0000/"; break;
+         case DYMuMu30_PbP:  return "DYtoMuMu_M-30_PbP-EmbEPOS_8p16_Powheg/crab_DYtoMuMu_M-30_PbP_Powheg_20170817/170817_152017/0000/"; break;
          case DYTauTau1030:return "DYtoMuMu_M-10to30_PbP-EmbEPOS_8p16_PowhegTauola/crab_DYtoTauTau_M-10to30_20180117_2/180117_132637/0000/"; break;
          case DYTauTau30:return "DYtoMuMu_M-30_PbP-EmbEPOS_8p16_PowhegTauola/crab_DYtoTauTau_M-30_20180117/180117_132803/0000/"; break;
          case WW:        return "WW_PbP-EmbEPOS_8p16_Pythia8/crab_WW_20180117/180117_132840/0000/"; break;
@@ -329,4 +329,4 @@ namespace DYana_v20180111_Pyquen {
    };
 };
 
-#endif // #ifndef samples_v20180111_Pyquen_h
+#endif // #ifndef samples_v20180111_h
