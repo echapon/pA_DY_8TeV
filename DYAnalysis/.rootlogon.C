8ac99fefda580ea9123967b0869bf1abe8bccdb7
Emilien Chapon <emilien.chapon@cern.ch>
2018-02-07 12:47:44 +0100

    Setup files

---
+++ DYAnalysis/.rootlogon.C
@@ -0,0 +1,10 @@
+{
+   // EC version
+   gSystem->SetIncludePath("-I/afs/cern.ch/user/e/echapon/workspace/private/xFitter/deps/lhapdf/include");
+   gSystem->AddLinkedLibs("/afs/cern.ch/user/e/echapon/workspace/private/xFitter/deps/lhapdf/lib/libLHAPDF.so");
+   printf("Setup done for Emilien's computer\n");
+
+   // CVMFS version (not working with ROOT <= 6.06, which is all I could find on lxplus...)
+   // gSystem->SetIncludePath("-I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/lhapdf/6.1.6-ikhhed/include/");
+   // gSystem->AddLinkedLibs("/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/lhapdf/6.1.6-ikhhed/lib/libLHAPDF.so");
+}
---
+++ DYAnalysis/setup_EC.sh
@@ -0,0 +1,5 @@
+export PATH=/home/emilien/Documents/FellowCERN/xFitter/deps/lhapdf/bin:$PATH
+export LD_LIBRARY_PATH=/home/emilien/Documents/FellowCERN/xFitter/deps/lhapdf/lib/:$LD_LIBRARY_PATH
+export LHAPDF_DATA_PATH=/home/emilien/Documents/FellowCERN/xFitter/deps/lhapdf/share/LHAPDF/
+
+echo "setup done for LHAPDF (WORKS ONLY ON EMILIEN'S COMPUTER)"
