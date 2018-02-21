d7614b7b25334196cfe7dc286f621679ccd11b8e
Emilien Chapon <emilien.chapon@cern.ch>
2018-02-21 13:40:42 +0100

    Update batch files

--- DYAnalysis/batch_acceff.sh
+++ DYAnalysis/batch_acceff.sh
@@ -6,8 +6,11 @@ cd $dir
 source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
 
+# nominal
 if [ $1 -eq 0 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both)'
+
+# alternative reweighting
 elif [ $1 -eq 1 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::Ntracks)'
 elif [ $1 -eq 2 ]; then
@@ -16,14 +19,22 @@ elif [ $1 -eq 3 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::minus)'
 elif [ $1 -eq 4 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",0,false)'
+
+# Pyquen
 elif [ $1 -eq 5 ]; then
    arg='(kFALSE,"Pyquen","PAL3Mu12",0,true,HFweight::HFside::both)'
+
+# pPb vs PbP
 elif [ $1 -eq 6 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both)'
-elif [ $1 -eq 7 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",1,true,HFweight::HFside::both)'
-elif [ $1 -eq 8 ]; then
+elif [ $1 -eq 7 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",2,true,HFweight::HFside::both)'
+
+# MomCor
+elif [ $1 -eq 8 ]; then
+   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both,0,0)'
+elif [ $1 -eq 9 ]; then
+   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both,7,6)'
 fi
 
 echo root -l -b -q AccEff/Acc_Eff.C+${arg}
--- DYAnalysis/batch_controlplots.sh
+++ DYAnalysis/batch_controlplots.sh
@@ -6,8 +6,11 @@ cd $dir
 source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
 
+# nominal
 if [ $1 -eq 0 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",true,HFweight::HFside::both,true)'
+
+# alternative reweighting
 elif [ $1 -eq 1 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",true,HFweight::HFside::both,false)'
 elif [ $1 -eq 2 ]; then
@@ -18,14 +21,24 @@ elif [ $1 -eq 4 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",true,HFweight::HFside::minus,true)'
 elif [ $1 -eq 5 ]; then
    arg='(kFALSE,"Powheg","PAL3Mu12",false)'
+
+# Pyquen
 elif [ $1 -eq 6 ]; then
    arg='(kFALSE,"Pyquen","PAL3Mu12",true,HFweight::HFside::both,true)'
+
+# MomCor
 elif [ $1 -eq 7 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",true,HFweight::HFside::both,true)'
+   arg='(kTRUE,"Powheg","PAL3Mu12",true,HFweight::HFside::both,true,0,0)'
 elif [ $1 -eq 8 ]; then
-   arg='(kFALSE,"Data")'
+   arg='(kTRUE,"Powheg","PAL3Mu12",true,HFweight::HFside::both,true,7,6)'
+
+# Data
 elif [ $1 -eq 9 ]; then
-   arg='(kTRUE,"Data")'
+   arg='(kFALSE,"Data")'
+elif [ $1 -eq 10 ]; then
+   arg='(kTRUE,"Data","PAL3Mu12",false,HFweight::HFside::both,false,0,0)'
+elif [ $1 -eq 11 ]; then
+   arg='(kTRUE,"Data","PAL3Mu12",false,HFweight::HFside::both,false,7,6)'
 fi
 
 echo root -l -b -q ControlPlots/MuonPlots.C+${arg}
