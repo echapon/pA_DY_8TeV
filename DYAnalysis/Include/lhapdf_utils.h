b39f7569b3af424d2f44246fe32fd2ac1764413f
Emilien Chapon <emilien.chapon@cern.ch>
2018-06-01 10:11:10 +0200

    Add improvements and more stuff

--- DYAnalysis/Include/lhapdf_utils.h
+++ DYAnalysis/Include/lhapdf_utils.h
@@ -5,6 +5,8 @@
 #include "TH1.h"
 #include "TH2.h"
 #include "TGraphAsymmErrors.h"
+#include "TCanvas.h"
+#include "TLegend.h"
 
 #include "LHAPDF/PDFSet.h"
 #include "LHAPDF/PDF.h"
@@ -104,6 +106,7 @@ TGraphAsymmErrors* hist2graph(TH1 *hist, double syst=0) {
 }
 
 // plot the pdf
+// NB: if pid>1000, we compute the valence contribution for pid-1000
 TH1D* hpdf(const char* pdfname, int imem, int pid, double q2, double xmin, double xmax, int npoints) {
    // make a constant binning in log(x)
    double *bins = new double[npoints+1];
@@ -121,7 +124,8 @@ TH1D* hpdf(const char* pdfname, int imem, int pid, double q2, double xmin, doubl
 
    // fill the histo
    for (int i=1; i<=npoints; i++) {
-      ans->SetBinContent(i,pdf->xfxQ2(pid,ans->GetBinCenter(i),q2));
+      if (pid<1000) ans->SetBinContent(i,pdf->xfxQ2(pid,ans->GetBinCenter(i),q2));
+      else  ans->SetBinContent(i,pdf->xfxQ2(pid-1000,ans->GetBinCenter(i),q2) - pdf->xfxQ2(-(pid-1000),ans->GetBinCenter(i),q2));
    }
 
    delete[] bins;
@@ -129,7 +133,7 @@ TH1D* hpdf(const char* pdfname, int imem, int pid, double q2, double xmin, doubl
 }
 
 // plot the pdf with uncertainties
-TGraphAsymmErrors* gpdf(const char* pdfname, int pid, double q2, double xmin, double xmax, int npoints, bool rel=false) {
+TGraphAsymmErrors* gpdf(const char* pdfname, int pid, double q2, double xmin, double xmax, int npoints, const char* refpdfname="") {
    PDFSet pdfset(pdfname);
    size_t nm = pdfset.size();
    vector<TH1D*> v;
@@ -138,6 +142,13 @@ TGraphAsymmErrors* gpdf(const char* pdfname, int pid, double q2, double xmin, do
    }
    TGraphAsymmErrors *ans = pdfuncert(v,pdfname);
 
+   TH1D *hrefpdf;
+   bool rel = (TString(refpdfname)!="");
+   if (rel) {
+      hrefpdf = hpdf(refpdfname,0,pid,q2,xmin,xmax,npoints);
+   }
+
+   // if a ref pdf is provided: divide the answer by it
    if (rel) {
       for (int i=0; i<ans->GetN(); i++) {
          double x = ans->GetX()[i];
@@ -146,8 +157,9 @@ TGraphAsymmErrors* gpdf(const char* pdfname, int pid, double q2, double xmin, do
          double exh = ans->GetEXhigh()[i];
          double eyl = ans->GetEYlow()[i];
          double eyh = ans->GetEYhigh()[i];
-         ans->SetPoint(i,x,1);
-         ans->SetPointError(i,exl,exh,eyl/y,eyh/y);
+         double yref = hrefpdf->GetBinContent(i+1);
+         ans->SetPoint(i,x,y/yref);
+         ans->SetPointError(i,exl,exh,eyl/yref,eyh/yref);
       }
    }
 
@@ -163,6 +175,165 @@ TH2D *testpdfcorr(const char* pdfname, int pid, double q2, double xmin, double x
       v.push_back(hpdf(pdfname,i,pid,q2,xmin,xmax,npoints));
    }
    return pdfcorr(v,pdfname);
+}
+
+// compare g, s, uv, dv, u, d, ubar, dbar in a single canvas
+// can provide a comma-separated list of pdf names
+void canvaspdfs(const char* pdfnames, double q2, double xmin, double xmax, int npoints, const char* refpdfname="") {
+   TString tpdfnames(pdfnames);
+
+   vector<int> colors;
+   colors.push_back(kBlue+1);
+   colors.push_back(kGreen+1);
+   colors.push_back(kYellow+1);
+   colors.push_back(kRed+1);
+   colors.push_back(kCyan+1);
+   colors.push_back(kMagenta+1);
+   vector<int> styles;
+   styles.push_back(3004);
+   styles.push_back(3005);
+   styles.push_back(3006);
+   styles.push_back(3007);
+   styles.push_back(3215);
+   styles.push_back(3275);
+
+   TCanvas *cpdf = new TCanvas("cpdf","cpdf",600,1200);
+   cpdf->Divide(2,4);
+   cpdf->cd(1);
+   gPad->SetLogx();
+
+   double x1=0.1, y1=0.7, x2=0.4, y2=0.9;
+   if (TString(refpdfname)=="") {x1=0.65; x2=0.95;}
+   TLegend *tleg = new TLegend(x1,y1,x2,y2);
+   tleg->SetBorderSize(0);
+
+   TString tok;
+   Ssiz_t from = 0;
+   int cnt=0;
+   int pid=21;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      tleg->AddEntry(g,tok,"LF");
+      cnt++;
+   }
+   tleg->Draw();
+
+   cpdf->cd(2);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=3;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
+
+   cpdf->cd(3);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=1002;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
+
+   cpdf->cd(4);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=1001;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
+
+   cpdf->cd(5);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=2;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
+
+   cpdf->cd(6);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=1;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
+
+   cpdf->cd(7);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=-2;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
+
+   cpdf->cd(8);
+   gPad->SetLogx();
+   tok="";
+   from = 0;
+   cnt=0;
+   pid=-1;
+   while (tpdfnames.Tokenize(tok, from, ",")) {
+      TGraphAsymmErrors *g = gpdf(tok, pid, q2, xmin, xmax, npoints, refpdfname);
+      g->SetLineColor(colors[cnt]);
+      g->SetFillColor(colors[cnt]);
+      g->SetFillStyle(styles[cnt]);
+      if (cnt==0) g->Draw("ALE3");
+      else g->Draw("LE3");
+      cnt++;
+   }
 }
 
 #endif // #ifndef lhapdf_utils_h
