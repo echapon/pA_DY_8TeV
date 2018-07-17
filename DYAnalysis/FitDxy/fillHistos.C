f04391b53676729443ecf19af37b17d81c791037
Emilien Chapon <emilien.chapon@cern.ch>
2018-07-17 17:14:09 +0200

    Add KDE stuff

--- DYAnalysis/FitDxy/fillHistos.C
+++ DYAnalysis/FitDxy/fillHistos.C
@@ -1,6 +1,16 @@
 #include "../BkgEst/interface/defs.h"
+#include "KDEProducer.cc"
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TDirectory.h"
+
+#include <iostream>
+#include <fstream>
 
 using namespace DYana;
+using namespace std;
 
 void fillHisto(TFile *fdata, TFile *fmc, 
       double massbin1, double massbin2,
@@ -9,6 +19,15 @@ void fillHisto(TFile *fdata, TFile *fmc,
       double phistarbin1, double phistarbin2);
 void printCombineDatacard(const char* filename, const char* histfilename, const char* dirname, 
       TH1D *data_obs, TH1D *DYMuMu, TH1D *DYTauTau, TH1D *htt, TH1D *hww, TH1D *hwz, TH1D *hzz, TH1D *DataSS1, TH1D *DataSS2);
+TH1D* fillHistoKDE(TFile *file, TString treename, TString histname,
+      bool doOS,
+      double massbin1, double massbin2,
+      double rapbin1, double rapbin2,
+      double ptbin1, double ptbin2,
+      double phistarbin1, double phistarbin2,
+      bool doweights, double hSF,
+      unsigned int nXBins, double xMin, double xMax, double trimFactor, bool doSigmaScaling,
+      bool doAdaptive);
 
 void fillHistos(const char* datafile, const char* mcfile, const char* outputfile,
       const char* expr="log(vtxnormchi2)/log(10)", int nvarbins=80, double varmin=-4, double varmax=4) {
@@ -238,12 +257,12 @@ void fillHisto(TFile *fdata, TFile *fmc,
    // data SS, same sign dxy
    // cout << "Filling data SS histo" << endl;
    tr = (TTree*) fdata->Get("tr_Data1");
-   tr->Draw("0.95*log(vtxnormchi2)/log(10)>>+DataSS1",
+   tr->Draw("log(vtxnormchi2)/log(10)>>+DataSS1",
          Form("sign!=0&&diMass>%f&&diMass<%f&&diRapidity-0.47>%f&&diRapidity-0.47<%f&&diPt>%f&&diPt<%f&&diPhistar>%f&&diPhistar<%f&&dxyVTX1*dxyVTX2/abs(dxyVTX1*dxyVTX2)>0",
             massbin1,massbin2,rapbin1,rapbin2,ptbin1,ptbin2,phistarbin1,phistarbin2),
          "goff");
    tr = (TTree*) fdata->Get("tr_Data2");
-   tr->Draw("0.95*log(vtxnormchi2)/log(10)>>+DataSS1",
+   tr->Draw("log(vtxnormchi2)/log(10)>>+DataSS1",
          Form("sign!=0&&diMass>%f&&diMass<%f&&diRapidity-0.47>%f&&diRapidity-0.47<%f&&diPt>%f&&diPt<%f&&diPhistar>%f&&diPhistar<%f&&dxyVTX1*dxyVTX2/abs(dxyVTX1*dxyVTX2)>0",
             massbin1,massbin2,rapbin1,rapbin2,ptbin1,ptbin2,phistarbin1,phistarbin2),
          "goff");
@@ -251,12 +270,12 @@ void fillHisto(TFile *fdata, TFile *fmc,
    // data SS, opposite sign dxy
    // cout << "Filling data SS histo" << endl;
    tr = (TTree*) fdata->Get("tr_Data1");
-   tr->Draw("0.95*log(vtxnormchi2)/log(10)>>+DataSS2",
+   tr->Draw("log(vtxnormchi2)/log(10)>>+DataSS2",
          Form("sign!=0&&diMass>%f&&diMass<%f&&diRapidity-0.47>%f&&diRapidity-0.47<%f&&diPt>%f&&diPt<%f&&diPhistar>%f&&diPhistar<%f&&dxyVTX1*dxyVTX2/abs(dxyVTX1*dxyVTX2)<=0",
             massbin1,massbin2,rapbin1,rapbin2,ptbin1,ptbin2,phistarbin1,phistarbin2),
          "goff");
    tr = (TTree*) fdata->Get("tr_Data2");
-   tr->Draw("0.95*log(vtxnormchi2)/log(10)>>+DataSS2",
+   tr->Draw("log(vtxnormchi2)/log(10)>>+DataSS2",
          Form("sign!=0&&diMass>%f&&diMass<%f&&diRapidity-0.47>%f&&diRapidity-0.47<%f&&diPt>%f&&diPt<%f&&diPhistar>%f&&diPhistar<%f&&dxyVTX1*dxyVTX2/abs(dxyVTX1*dxyVTX2)<=0",
             massbin1,massbin2,rapbin1,rapbin2,ptbin1,ptbin2,phistarbin1,phistarbin2),
          "goff");
@@ -284,4 +303,70 @@ void printCombineDatacard(const char* filename, const char* histfilename, const 
    of << "nDataSS2 rateParam " << binname << " DataSS2 @0*(1-@1)/" << DataSS2->Integral() << " nDataSS,fracDataSS1" << endl;
    of << "nDataSS extArg " << DataSS1->Integral()+DataSS2->Integral() << " [0," << 100*(DataSS1->Integral()+DataSS2->Integral()) << "]" << endl;
    of << "fracDataSS1 extArg 0.5 [0,1] " << endl;
+}
+
+TH1D* fillHistoKDE(TFile *file, TString treename, TString histname,
+      bool doOS,
+      double massbin1, double massbin2,
+      double rapbin1, double rapbin2,
+      double ptbin1, double ptbin2,
+      double phistarbin1, double phistarbin2,
+      int dxymode,
+      bool doweights, double hSF,
+      unsigned int nXBins, double xMin, double xMax, double trimFactor, bool doSigmaScaling,
+      bool doAdaptive
+      ) {
+   // vectors of values
+   vector<double> vals, weights;
+   int nevts=0;
+
+   // loop on the tree(s)
+   TString tok;
+   Ssiz_t from = 0;
+   while (treename.Tokenize(tok, from, ",")) {
+      TTree *tr = (TTree*) file->Get(tok);
+      // variables
+      int sign;
+      float diMass, diRapidity, diPt, diPhistar, vtxnormchi2;
+      float dxyVTX1,dxyVTX2;
+      float weight=1;
+      tr->SetBranchStatus("*",0);
+      tr->SetBranchStatus("sign",1); tr->SetBranchAddress("sign",&sign);
+      tr->SetBranchStatus("diMass",1); tr->SetBranchAddress("diMass",&diMass);
+      tr->SetBranchStatus("diRapidity",1); tr->SetBranchAddress("diRapidity",&diRapidity);
+      tr->SetBranchStatus("diPt",1); tr->SetBranchAddress("diPt",&diPt);
+      tr->SetBranchStatus("diPhistar",1); tr->SetBranchAddress("diPhistar",&diPhistar);
+      tr->SetBranchStatus("vtxnormchi2",1); tr->SetBranchAddress("vtxnormchi2",&vtxnormchi2);
+      if (dxymode>0) {
+         tr->SetBranchStatus("dxyVTX1",1); tr->SetBranchAddress("dxyVTX1",&dxyVTX1);
+         tr->SetBranchStatus("dxyVTX2",1); tr->SetBranchAddress("dxyVTX2",&dxyVTX2);
+      }
+      if (doweights) {
+         tr->SetBranchStatus("weight",1); tr->SetBranchAddress("weight",&weight);
+      }
+
+      for (int i=0; i<tr->GetEntries(); i++) {
+         tr->GetEntry(i);
+         if (doOS && sign!=0) continue;
+         if (!doOS && sign==0) continue;
+         if (!(diMass>massbin1 && diMass<massbin2)) continue;
+         if (!(diRapidity>rapbin1 && diRapidity<rapbin2)) continue;
+         if (!(diPt>ptbin1 && diPt<ptbin2)) continue;
+         if (!(diPhistar>phistarbin1 && diPhistar<phistarbin2)) continue;
+         if (dxymode==1 && !(dxyVTX1*dxyVTX2/abs(dxyVTX1*dxyVTX2)>0)) continue;
+         if (dxymode==2 && !(dxyVTX1*dxyVTX2/abs(dxyVTX1*dxyVTX2)<=0)) continue;
+         double val = log(vtxnormchi2)/log(10);
+         vals.push_back(val);
+         weights.push_back(weight);
+         if (val>xMin&&val<xMax) nevts++;
+      } // end event loop
+   } // end tokenize
+
+   KDEProducer kdeprod(&vals,&weights,hSF,nXBins,xMin,xMax,trimFactor,doSigmaScaling);
+
+   TH1D *ans = NULL;
+   if (doAdaptive) ans = (TH1D*) kdeprod.getAPDF(histname.Data(), "KDE density", nXBins, xMin, xMax);
+   else ans = (TH1D*) kdeprod.getPDF(histname.Data(), "KDE density", nXBins, xMin, xMax);
+   ans->Scale(nevts*((xMax-xMin)/nXBins));
+   return ans;
 }
