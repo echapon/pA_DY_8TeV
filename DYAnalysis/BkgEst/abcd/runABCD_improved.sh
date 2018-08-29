b728c185ecf4d7efaf0290bff92e23e04f84a7da
Emilien Chapon <emilien.chapon@cern.ch>
2018-08-29 18:30:40 +0200

    Improved ABCD accounting for Wjets (does not work)

---
+++ DYAnalysis/BkgEst/abcd/runABCD_improved.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# # fill histos
+# root -l -b -q fillHistograms.C'("Data","PAL1DoubleMu0")' 
+# root -l -b -q fillHistograms.C'("Powheg","PAL1DoubleMu0")' 
+# root -l -b -q fillHistograms.C'("Data","PAL3Mu12")' 
+# root -l -b -q fillHistograms.C'("Powheg","PAL3Mu12")' 
+
+# run ABCD method
+root -l -b -q runABCD_improved.C'("PAL1DoubleMu0","DataMinusMC")'
+root -l -b -q runABCD_improved.C'("PAL1DoubleMu0","QCD")'
+root -l -b -q runABCD_improved.C'("PAL3Mu12","DataMinusMC")'
+root -l -b -q runABCD_improved.C'("PAL3Mu12","QCD")'
+
+# make plots
+root -l -b -q plotABCD.C'("PAL1DoubleMu0","DataMinusMC")'
+root -l -b -q plotABCD.C'("PAL1DoubleMu0","QCD")'
+root -l -b -q plotABCD.C'("PAL3Mu12","DataMinusMC")'
+root -l -b -q plotABCD.C'("PAL3Mu12","QCD")'
