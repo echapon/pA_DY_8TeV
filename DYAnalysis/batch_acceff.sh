bdcfcaefc93e00cfc9ac96285f6a1696cc13a137
Emilien Chapon <emilien.chapon@cern.ch>
2019-01-23 14:14:40 +0100

    2 different triggers

--- DYAnalysis/batch_acceff.sh
+++ DYAnalysis/batch_acceff.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# first argument, from 0 to 10: variation
+
+# trigger=PAL3Mu12
+trigger=PAL1DoubleMu0
+
 dir=/afs/cern.ch/user/e/echapon/workspace/private/2016_pPb/DY/tree_ana/PADrellYan8TeV/DYAnalysis
 
 cd $dir
@@ -8,37 +13,37 @@ source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/
 
 # nominal
 if [ $1 -eq 0 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both,0,0)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,true,HFweight::HFside::both,0,0)'
 
 # alternative reweighting
 elif [ $1 -eq 1 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::Ntracks)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,true,HFweight::HFside::Ntracks)'
 elif [ $1 -eq 2 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::plus)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,true,HFweight::HFside::plus)'
 elif [ $1 -eq 3 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::minus)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,true,HFweight::HFside::minus)'
 elif [ $1 -eq 4 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,false)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,false)'
 
 # Pyquen
 elif [ $1 -eq 5 ]; then
-   arg='(kTRUE,"Pyquen","PAL3Mu12",0,true,HFweight::HFside::both)'
+   arg='(kTRUE,"Pyquen","'$trigger'",0,true,HFweight::HFside::both)'
 
 # pPb vs PbP
 elif [ $1 -eq 6 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",1,true,HFweight::HFside::both)'
+   arg='(kTRUE,"Powheg","'$trigger'",1,true,HFweight::HFside::both)'
 elif [ $1 -eq 7 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",2,true,HFweight::HFside::both)'
+   arg='(kTRUE,"Powheg","'$trigger'",2,true,HFweight::HFside::both)'
 
 # MomCor
 elif [ $1 -eq 8 ]; then # no correction
-   arg='(kFALSE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both)'
+   arg='(kFALSE,"Powheg","'$trigger'",0,true,HFweight::HFside::both)'
 elif [ $1 -eq 9 ]; then # Run2016H correction
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both,7,6)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,true,HFweight::HFside::both,7,6)'
 
 # no Zpt reweighting
 elif [ $1 -eq 10 ]; then
-   arg='(kTRUE,"Powheg","PAL3Mu12",0,true,HFweight::HFside::both,0,0,false)'
+   arg='(kTRUE,"Powheg","'$trigger'",0,true,HFweight::HFside::both,0,0,false)'
 fi
 
 echo root -l -b -q AccEff/Acc_Eff.C+${arg}
