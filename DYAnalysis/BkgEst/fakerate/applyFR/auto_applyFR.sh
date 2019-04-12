f4c7a2b543861f8468a01cce9466be3887e00271
HyunchulKim <hyunchul.kim@cern.ch>
2019-04-12 10:43:25 +0200

    update header files mainly

---
+++ DYAnalysis/BkgEst/fakerate/applyFR/auto_applyFR.sh
@@ -0,0 +1,10 @@
+
+mkdir histograms_20190411v12_rebin0326
+
+root -l -b -q 'applyFR.cc++("../estimateFR/resultEst_20190411v11_iso0_1_HFminus/fakerate_reltrkisoR03muptlt10_isomax0p2_opt1050_QCDopt2_L3Mu12.root","opt1050_QCDopt2_reltrkisoR03muptlt10_isomax0p2_L3Mu12","20190411v12_rebin0326")'
+
+
+
+
+
+
