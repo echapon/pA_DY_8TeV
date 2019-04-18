ac1f2832b4c5adab9f0b12cab6f0e59c65ac2c67
Emilien Chapon <emilien.chapon@cern.ch>
2019-04-18 16:01:25 +0200

    New working version

--- DYAnalysis/BkgEst/abcd/runABCD_improved2.C
+++ DYAnalysis/BkgEst/abcd/runABCD_improved2.C
@@ -2,6 +2,10 @@
 
 #include "TFile.h"
 #include "TH1.h"
+#include "TVirtualFitter.h"
+#include "TGraphErrors.h"
+#include "TCanvas.h"
+#include "TFitResult.h"
 
 const TString isotag="0.0-0.2";
 const TString nonisotag="0.2-10.0";
@@ -10,7 +14,7 @@ using namespace std;
 using namespace DYana;
 using namespace DYana_v20180814;
 
-void runABCD_improved2(var thevar, const char* fdata="ROOTFile_Histogram_InvMass_isos_PAL3Mu12_Data_MomCorr00_noHFrew_notnprew.root", const char* fMC="ROOTFile_Histogram_InvMass_isos_PAL3Mu12_Powheg_MomCorr00_rewboth_tnprew.root") {
+void runABCD_improved2(var thevar, const char* fdata="ROOTFile_Histogram_InvMass_isos_PAL3Mu12_Data_MomCorr00_noHFrew_notnprew.root", const char* fMC="ROOTFile_Histogram_InvMass_isos_PAL3Mu12_Powheg_MomCorr00_rewboth_tnprew.root", double SSfactor=2.15824, double SSfactor_err=0.1) {
    TFile *tfdata = TFile::Open(fdata);
    TFile *tfMC = TFile::Open(fMC);
 
@@ -211,8 +215,8 @@ void runABCD_improved2(var thevar, const char* fdata="ROOTFile_Histogram_InvMass
    h_SS_2iso[WFirst]->Write("h_SS_2iso_WJets");
    h_SS_2iso[ALL]->Write("h_SS_2iso_QCDMC");
 
-   // use 0iso and 1iso to determine SS and Wjets scaling factors, then use this to go to 2iso
-   // fit 0iso
+   // use 0iso and 1iso to determine SS scaling factors (assuming Wjets is OK -- it's sub-dominant anyway), then use this to go to 2iso
+   ///// 0iso
    // start by subtracting Wjets and EW from SS data
    h_SS_0iso[Data1]->Add(h_SS_0iso[0],-1);
    h_SS_0iso[Data1]->Add(h_SS_0iso[WFirst],-1);
@@ -226,41 +230,16 @@ void runABCD_improved2(var thevar, const char* fdata="ROOTFile_Histogram_InvMass
    double fracWj_0iso = int0_Wj_0iso/h_OS_0iso[Data1]->Integral();
 
    // scale SS data template so that the sum is equal to the data integral
+   double iOS, iSS, eOS, eSS;
+   iOS = h_OS_0iso[Data1]->IntegralAndError(1,nbinsvar(thevar),eOS);
+   iSS = h_SS_0iso[Data1]->IntegralAndError(1,nbinsvar(thevar),eSS);
    h_SS_0iso[Data1]->Scale((1.-fracEW_0iso-fracWj_0iso) * h_OS_0iso[Data1]->Integral() / h_SS_0iso[Data1]->Integral());
    double int1_SS_0iso = h_SS_0iso[Data1]->Integral();
-   double fracSS1_0iso = int1_SS_0iso/h_OS_0iso[Data1]->Integral();
+   double fact_0iso = int1_SS_0iso / int0_SS_0iso;
+   double fact_err_0iso = fact_0iso * sqrt(pow(eOS/iOS,2) + pow(eSS/iSS,2));
+   cout << "SS scaling factor for 0iso: " << fact_0iso << " +/- " << fact_err_0iso << endl;
 
-   TObjArray *templates_0iso = new TObjArray(3);
-   templates_0iso->Add(h_SS_0iso[Data1]);
-   templates_0iso->Add(h_OS_0iso[0]);
-   templates_0iso->Add(h_OS_0iso[WFirst]);
-   TFractionFitter *fit0iso = new TFractionFitter(h_OS_0iso[Data1],templates_0iso);
-
-   fit0iso->Constrain(0, 0.1*fracSS1_0iso, 10.*fracSS1_0iso); // SS data can float more (expectation: around 0.5
-   fit0iso->Constrain(1, 0.9*fracEW_0iso, 1.1*fracEW_0iso); // EW cannot float much
-   fit0iso->Constrain(2, 0.5*fracWj_0iso, 1.5*fracWj_0iso); // Wjets can float a bit
-
-   Int_t status = fit0iso->Fit();               // perform the fit
-   std::cout << "fit status: " << status << std::endl;
-   if (status == 0) {                       // check on fit status
-      TH1D* result_0iso = (TH1D*) fit0iso->GetPlot();
-      h_OS_0iso[Data1]->Draw("Ep");
-      result_0iso->Draw("same");
-      c1.SaveAs("test_fit_0iso.pdf");
-   }
-
-   // compare fractions before and after fit
-   cout << "Initial: " << fracSS_0iso << " data SS, " << fracEW_0iso << " EW, " << fracWj_0iso << " Wjets." << endl;
-   cout << "Before fit: " << fracSS1_0iso << " data SS, " << fracEW_0iso << " EW, " << fracWj_0iso << " Wjets." << endl;
-   double fracfitSS_0iso, fracfiterrSS_0iso;
-   fit0iso->GetResult(0, fracfitSS_0iso, fracfiterrSS_0iso);
-   double fracfitEW_0iso, fracfiterrEW_0iso;
-   fit0iso->GetResult(1, fracfitEW_0iso, fracfiterrEW_0iso);
-   double fracfitWj_0iso, fracfiterrWj_0iso;
-   fit0iso->GetResult(2, fracfitWj_0iso, fracfiterrWj_0iso);
-   cout << "After fit: " << fracfitSS_0iso << " data SS, " << fracfitEW_0iso << " EW, " << fracfitWj_0iso << " Wjets." << endl;
-
-   // fit 1iso
+   ///// 1iso
    // start by subtracting Wjets and EW from SS data
    h_SS_1iso[Data1]->Add(h_SS_1iso[0],-1);
    h_SS_1iso[Data1]->Add(h_SS_1iso[WFirst],-1);
@@ -274,39 +253,35 @@ void runABCD_improved2(var thevar, const char* fdata="ROOTFile_Histogram_InvMass
    double fracWj_1iso = int0_Wj_1iso/h_OS_1iso[Data1]->Integral();
 
    // scale SS data template so that the sum is equal to the data integral
+   iOS = h_OS_1iso[Data1]->IntegralAndError(1,nbinsvar(thevar),eOS);
+   iSS = h_SS_1iso[Data1]->IntegralAndError(1,nbinsvar(thevar),eSS);
    h_SS_1iso[Data1]->Scale((1.-fracEW_1iso-fracWj_1iso) * h_OS_1iso[Data1]->Integral() / h_SS_1iso[Data1]->Integral());
    double int1_SS_1iso = h_SS_1iso[Data1]->Integral();
-   double fracSS1_1iso = int1_SS_1iso/h_OS_1iso[Data1]->Integral();
+   double fact_1iso = int1_SS_1iso / int0_SS_1iso;
+   double fact_err_1iso = fact_1iso * sqrt(pow(eOS/iOS,2) + pow(eSS/iSS,2));
+   cout << "SS scaling factor for 1iso: " << fact_1iso << " +/- " << fact_err_1iso << endl;
 
-   TObjArray *templates_1iso = new TObjArray(3);
-   templates_1iso->Add(h_SS_1iso[Data1]);
-   templates_1iso->Add(h_OS_1iso[0]);
-   templates_1iso->Add(h_OS_1iso[WFirst]);
-   TFractionFitter *fit1iso = new TFractionFitter(h_OS_1iso[Data1],templates_1iso);
+   // average 0iso and 1iso
+   // 2 versions: simple weighted average, or extrapolation to 2iso for possible dependence
+   double xx[2] = {0,1};
+   double ex[2] = {0,0};
+   double yy[2] = {fact_0iso,fact_1iso};
+   double ey[2] = {fact_err_0iso,fact_err_1iso};
+   TGraphErrors tg(2,xx,yy,ex,ey);
 
-   fit1iso->Constrain(0, 0.1*fracSS1_1iso, 10.*fracSS1_1iso); // SS data can float more (expectation: around 0.5
-   fit1iso->Constrain(1, 0.9*fracEW_1iso, 1.1*fracEW_1iso); // EW cannot float much
-   fit1iso->Constrain(2, 0.5*fracWj_1iso, 1.5*fracWj_1iso); // Wjets can float a bit
+   TFitResultPtr resa = tg.Fit("pol0","MENS EX0");
+   double fact_a = resa->Value(0);
+   double fact_err_a = resa->ParError(0);
 
-   status = fit1iso->Fit();               // perform the fit
-   std::cout << "fit status: " << status << std::endl;
-   if (status == 0) {                       // check on fit status
-      TH1D* result_1iso = (TH1D*) fit1iso->GetPlot();
-      h_OS_1iso[Data1]->Draw("Ep");
-      result_1iso->Draw("same");
-      c1.SaveAs("test_fit_1iso.pdf");
-   }
+   TFitResultPtr resb = tg.Fit("pol1","MENS EX0");
+   const double xxb[1] = {2};
+   double yyb[1] = {0};
+   resb->GetConfidenceIntervals(1,1,1,xxb,yyb,0.683);
+   double fact_b = resb->Value(0) + 2.*resb->Value(1);
+   double fact_err_b = yyb[0];
 
-   // compare fractions before and after fit
-   cout << "Initial: " << fracSS_1iso << " data SS, " << fracEW_1iso << " EW, " << fracWj_1iso << " Wjets." << endl;
-   cout << "Before fit: " << fracSS1_1iso << " data SS, " << fracEW_1iso << " EW, " << fracWj_1iso << " Wjets." << endl;
-   double fracfitSS_1iso, fracfiterrSS_1iso;
-   fit1iso->GetResult(0, fracfitSS_1iso, fracfiterrSS_1iso);
-   double fracfitEW_1iso, fracfiterrEW_1iso;
-   fit1iso->GetResult(1, fracfitEW_1iso, fracfiterrEW_1iso);
-   double fracfitWj_1iso, fracfiterrWj_1iso;
-   fit1iso->GetResult(2, fracfitWj_1iso, fracfiterrWj_1iso);
-   cout << "After fit: " << fracfitSS_1iso << " data SS, " << fracfitEW_1iso << " EW, " << fracfitWj_1iso << " Wjets." << endl;
+   cout << "combined factor from averaging: " << fact_a << " +/- " << fact_err_a << endl;
+   cout << "combined factor from extrapolation to 2iso: " << fact_b << " +/- " << fact_err_b << endl;
 
    fout->Close();
 }
