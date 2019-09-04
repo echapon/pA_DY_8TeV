58781269c69a2f4c24603434d444575808bae13a
Emilien Chapon <emilien.chapon@cern.ch>
2019-09-04 14:57:23 +0200

    move to MyCanvas

--- DYAnalysis/AccEff/compareAccEff.C
+++ DYAnalysis/AccEff/compareAccEff.C
@@ -1,13 +1,14 @@
 #include "TFile.h"
 #include "TCanvas.h"
 #include "TEfficiency.h"
+#include "../Include/MyCanvas.C"
 
 void setStyle(TEfficiency *e, Color_t color=kBlack, Style_t markerstyle=kFullCircle);
 
 void compareAccEff(TString variable="Mass") { // Mass | Pt | Rap1560 | Rap60120 | Phistar
    TFile *fnom = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_rewboth.root");
-   TFile *fmomcor = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomCorr76_Powheg_PAL3Mu12_0_rewboth.root");
-   TFile *fnorew = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomUnCorr_Powheg_PAL3Mu12_0_rewboth.root");
+   TFile *fmomcor = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomUnCorr_Powheg_PAL3Mu12_0_rewboth.root");
+   TFile *fnorew = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_norew.root");
    TFile *frewminus = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_rewminus.root");
    TFile *frewplus = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_rewplus.root");
    TFile *fpPb = TFile::Open("ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_1_rewboth.root");
@@ -51,154 +52,91 @@ void compareAccEff(TString variable="Mass") { // Mass | Pt | Rap1560 | Rap60120 
    } else if (variable.Contains("Pt")) {
       xtitle="Gen-Level Dimuon p_{T} (post-FSR) [GeV]";
       xmin=0.3;
-      xmax=200.;
-   } else if (variable=="Rap1560") {
+      xmax=600.;
+   } else if (variable.Contains("Rap")) {
       xtitle="Gen-Level Dimuon y_{CM} (post-FSR)";
-      xmin=-3.;
-      xmax=2.;
-   } else if (variable=="Rap60120") {
-      xtitle="Gen-Level Dimuon y_{CM} (post-FSR)";
-      xmin=-3.;
-      xmax=2.;
+      xmin=-2.87;
+      xmax=1.93;
    } else if (variable.Contains("Phistar")) {
       xtitle="Gen-Level Dimuon #phi^{*} (post-FSR)";
       xmin=0.002;
-      xmax=3;
+      xmax=3.277;
    }
-   TH1F *haxes_acc = new TH1F("haxes_acc",";"+xtitle+";Acceptance",1,xmin,xmax);
-   haxes_acc->GetYaxis()->SetRangeUser(1e-3,1.1);
-   TH1F *haxes_eff = new TH1F("haxes_eff",";"+xtitle+";Efficiency",1,xmin,xmax);
-   haxes_eff->GetYaxis()->SetRangeUser(0.5,1.05);
 
-   // set styles
-   setStyle(acc_nom);
-   setStyle(eff_nom);
-   setStyle(acc_momcor,kRed,kFullSquare);
-   setStyle(eff_momcor,kRed,kFullSquare);
-   setStyle(acc_norew,kRed,kFullSquare);
-   setStyle(eff_norew,kRed,kFullSquare);
-   setStyle(acc_rewplus,kBlue,kFullTriangleUp);
-   setStyle(eff_rewplus,kBlue,kFullTriangleUp);
-   setStyle(acc_rewminus,kGreen,kFullTriangleDown);
-   setStyle(eff_rewminus,kGreen,kFullTriangleDown);
-   setStyle(acc_pPb,kRed,kFullTriangleUp);
-   setStyle(eff_pPb,kRed,kFullTriangleUp);
-   setStyle(acc_Pbp,kBlue,kFullTriangleDown);
-   setStyle(eff_Pbp,kBlue,kFullTriangleDown);
-   setStyle(acc_Pyquen,kRed,kFullSquare);
-   setStyle(eff_Pyquen,kRed,kFullSquare);
-
-   TCanvas *c1 = new TCanvas("c1","c1",600,600);
-   TLegend *tleg = NULL;
-   double xl1=0.6,yl1=0.2,xl2=0.9,yl2=0.5;
-   if (variable.Contains("Rap")) {xl1=0.35;xl2=0.65;}
-   if (variable == "Rap1560") {yl1=0.6;yl2=0.9;}
-
+   double xl1=0.6,yl1=0.35,xl2=0.9,yl2=0.55;
+   if (variable.Contains("Rap")) {xl1=0.35;xl2=0.7;}
+   if (variable == "Rap1560") {yl1=0.6;yl2=0.95;}
+   
    // Rochester corrections
-   haxes_eff->Draw();
-   eff_nom->Draw("same");
-   if (!variable.Contains("Rap")) gPad->SetLogx();
-   gPad->SetLogy(kFALSE);
-   // eff_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(0.5,1.1);
-   eff_momcor->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(eff_nom,"Mom. corr. B-H","lp");
-   tleg->AddEntry(eff_momcor,"Mom. corr. H","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_eff_momcor_InclVsH_"+variable+".pdf");
-   // return;
+   MyCanvas cEffRoch("comp_eff_momcor_InclVsH_"+variable,xtitle,"Efficiency");
+   if (!variable.Contains("Rap")) cEffRoch.SetLogx();
+   cEffRoch.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cEffRoch.SetXRange(xmin,xmax);
+   cEffRoch.SetYRange(0.5,1.05);
+   cEffRoch.SetRatioRange(0.95,1.05);
+   cEffRoch.CanvasWithGraphRatioPlot(eff_momcor->CreateGraph(),eff_nom->CreateGraph(),"No mom. corr.","Mom. corr. B-H","No mom. corr. / corr. B-H");
+   cEffRoch.PrintCanvas();
+
    
    // reweighting effect
-   haxes_acc->Draw();
-   acc_nom->Draw("same");
-   if (!variable.Contains("Rap")) gPad->SetLogx();
-   if (variable=="Mass") gPad->SetLogy();
-   // acc_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(1e-3,1.1);
-   acc_norew->Draw("same");
-   // acc_rewplus->Draw("same");
-   // acc_rewminus->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(acc_nom,"HF both","lp");
-   tleg->AddEntry(acc_norew,"No HF rew","lp");
-   // tleg->AddEntry(acc_rewplus,"HF+ rew","lp");
-   // tleg->AddEntry(acc_rewminus,"HF- rew","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_acc_rew_"+variable+".pdf");
+   MyCanvas cAccrew("comp_acc_rew_"+variable,xtitle,"Acciciency");
+   if (!variable.Contains("Rap")) cAccrew.SetLogx();
+   cAccrew.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cAccrew.SetXRange(xmin,xmax);
+   cAccrew.SetYRange(1e-3,1.1);
+   cAccrew.SetRatioRange(0.95,1.05);
+   cAccrew.CanvasWithGraphRatioPlot(acc_norew->CreateGraph(),acc_nom->CreateGraph(),"No HF rew","HF both","No HF rew / HF both");
+   cAccrew.PrintCanvas();
 
-   haxes_eff->Draw();
-   eff_nom->Draw("same");
-   gPad->SetLogy(kFALSE);
-   // eff_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(0.5,1.1);
-   eff_norew->Draw("same");
-   // eff_rewplus->Draw("same");
-   // eff_rewminus->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(eff_nom,"HF both","lp");
-   tleg->AddEntry(eff_norew,"No HF rew","lp");
-   // tleg->AddEntry(eff_rewplus,"HF+ rew","lp");
-   // tleg->AddEntry(eff_rewminus,"HF- rew","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_eff_rew_"+variable+".pdf");
-   
+   MyCanvas cEffrew("comp_eff_rew_"+variable,xtitle,"Efficiency");
+   if (!variable.Contains("Rap")) cEffrew.SetLogx();
+   cEffrew.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cEffrew.SetXRange(xmin,xmax);
+   cEffrew.SetYRange(0.5,1.05);
+   cEffrew.SetRatioRange(0.95,1.05);
+   cEffrew.CanvasWithGraphRatioPlot(eff_norew->CreateGraph(),eff_nom->CreateGraph(),"No HF rew","HF both","No HF rew / HF both");
+   cEffrew.PrintCanvas();
+
    // pPb vs Pbp
-   haxes_acc->Draw();
-   acc_nom->Draw("same");
-   if (variable=="Mass") gPad->SetLogy();
-   // acc_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(1e-3,1.1);
-   acc_pPb->Draw("same");
-   acc_Pbp->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(acc_nom,"pPb+Pbp","lp");
-   tleg->AddEntry(acc_pPb,"pPb","lp");
-   tleg->AddEntry(acc_Pbp,"Pbp","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_acc_beamdir_"+variable+".pdf");
+   MyCanvas cAccbeam("comp_acc_beamdir_"+variable,xtitle,"Acciciency");
+   if (!variable.Contains("Rap")) cAccbeam.SetLogx();
+   cAccbeam.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cAccbeam.SetXRange(xmin,xmax);
+   cAccbeam.SetYRange(1e-3,1.1);
+   cAccbeam.SetRatioRange(0.95,1.05);
+   cAccbeam.CanvasWithThreeGraphsRatioPlot(acc_pPb->CreateGraph(),acc_Pbp->CreateGraph(),acc_nom->CreateGraph(),"pPb","Pbp","pPb+Pbp","pPb or Pbp / pPb+Pbp");
+   cAccbeam.PrintCanvas();
 
-   haxes_eff->Draw();
-   eff_nom->Draw("same");
-   gPad->SetLogy(kFALSE);
-   // eff_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(0.5,1.1);
-   eff_pPb->Draw("same");
-   eff_Pbp->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(eff_nom,"pPb+Pbp","lp");
-   tleg->AddEntry(eff_pPb,"pPb","lp");
-   tleg->AddEntry(eff_Pbp,"Pbp","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_eff_beamdir_"+variable+".pdf");
+   MyCanvas cEffbeam("comp_eff_beamdir_"+variable,xtitle,"Efficiency");
+   if (!variable.Contains("Rap")) cEffbeam.SetLogx();
+   cEffbeam.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cEffbeam.SetXRange(xmin,xmax);
+   cEffbeam.SetYRange(0.5,1.05);
+   cEffbeam.SetRatioRange(0.95,1.05);
+   cEffbeam.CanvasWithThreeGraphsRatioPlot(eff_pPb->CreateGraph(),eff_Pbp->CreateGraph(),eff_nom->CreateGraph(),"pPb","Pbp","pPb+Pbp","pPb or Pbp / pPb+Pbp");
+   cEffbeam.PrintCanvas();
    
    // Powheg vs Pyquen
-   haxes_acc->Draw();
-   acc_nom->Draw("same");
-   if (variable=="Mass") gPad->SetLogy();
-   // acc_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(1e-3,1.1);
-   acc_Pyquen->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(acc_nom,"Powheg","lp");
-   tleg->AddEntry(acc_Pyquen,"Pyquen","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_acc_generator_"+variable+".pdf");
+   MyCanvas cAccgenerator("comp_acc_generator_"+variable,xtitle,"Acciciency");
+   if (!variable.Contains("Rap")) cAccgenerator.SetLogx();
+   cAccgenerator.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cAccgenerator.SetXRange(xmin,xmax);
+   cAccgenerator.SetYRange(1e-3,1.1);
+   cAccgenerator.SetRatioRange(0.95,1.05);
+   cAccgenerator.CanvasWithGraphRatioPlot(acc_Pyquen->CreateGraph(),acc_nom->CreateGraph(),"Pyquen","Powheg","Pyquen / Powheg");
+   cAccgenerator.PrintCanvas();
 
-   haxes_eff->Draw();
-   eff_nom->Draw("same");
-   gPad->SetLogy(kFALSE);
-   // eff_nom->GetPaintedGraph()->GetHistogram()->GetYaxis()->SetRangeUser(0.5,1.1);
-   eff_Pyquen->Draw("same");
-   tleg = new TLegend(xl1,yl1,xl2,yl2);
-   tleg->SetBorderSize(0);
-   tleg->AddEntry(eff_nom,"Powheg","lp");
-   tleg->AddEntry(eff_Pyquen,"Pyquen","lp");
-   tleg->Draw();
-   c1->SaveAs("comp_eff_generator_"+variable+".pdf");
+   MyCanvas cEffgenerator("comp_eff_generator_"+variable,xtitle,"Efficiency");
+   if (!variable.Contains("Rap")) cEffgenerator.SetLogx();
+   cEffgenerator.SetLegendPosition(xl1,yl1,xl2,yl2);
+   cEffgenerator.SetXRange(xmin,xmax);
+   cEffgenerator.SetYRange(0.5,1.05);
+   cEffgenerator.SetRatioRange(0.95,1.05);
+   cEffgenerator.CanvasWithGraphRatioPlot(eff_Pyquen->CreateGraph(),eff_nom->CreateGraph(),"Pyquen","Powheg","Pyquen / Powheg");
+   cEffgenerator.PrintCanvas();
 }
 
-void setStyle(TEfficiency *e, Color_t color=kBlack, Style_t markerstyle=kFullCircle) {
+void setStyle(TEfficiency *e, Color_t color, Style_t markerstyle) {
    e->SetMarkerColor(color);
    e->SetLineColor(color);
    e->SetMarkerStyle(markerstyle);
