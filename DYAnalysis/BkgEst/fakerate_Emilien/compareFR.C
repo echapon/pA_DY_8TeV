4aba364af5f19b731f1ddcde4432e2a7984f000d
Emilien Chapon <emilien.chapon@cern.ch>
2019-09-18 16:10:25 +0200

    Update to latest FR

--- DYAnalysis/BkgEst/fakerate_Emilien/compareFR.C
+++ DYAnalysis/BkgEst/fakerate_Emilien/compareFR.C
@@ -5,16 +5,27 @@ void compareFR(TString etaregion) { // "", "_barrel", "_endcap"
    TFile *f2muSS = TFile::Open("FRhistos_2muSS.root");
    TFile *f2muSS_lochi2 = TFile::Open("FRhistos_2muSS_lochi2.root");
    TFile *f2muSS_hichi2 = TFile::Open("FRhistos_2muSS_hichi2.root");
-   TFile *ftemplate = TFile::Open("/afs/cern.ch/work/h/hckim/public/ForEmilien_20190506_v2/input/estimateFRinput/fakerate_reltrkisoR03muptlt10isomax0p2_opt1050_QCDopt2_L3Mu12_beforeCor.root");
+   // TFile *ftemplate = TFile::Open("/afs/cern.ch/work/h/hckim/public/ForEmilien_20190506_v2/input/estimateFRinput/fakerate_reltrkisoR03muptlt10isomax0p2_opt1050_QCDopt2_L3Mu12_beforeCor.root");
+   // TFile *ftemplate = TFile::Open("/afs/cern.ch/work/h/hckim/public/ForEmilien_20190514");
+   // TFile *ftemplate = TFile::Open("/afs/cern.ch/work/h/hckim/public/ForEmilien_20190514/FRfinal/fakerate_reltrkisoR03muptlt10isomax0p2_opt1050_QCDopt2_L3Mu12.root");
+   TFile *ftemplate = TFile::Open("/afs/cern.ch/work/e/echapon/public/DY_pA_2016/FR_HCK_v20190918/fakerate_QCDopt3_ptincl_correctedFR.root");
 
-   TH1D *h1mu = (TH1D*) f1mu->Get("hFR" + etaregion);
-   TH1D *h2muSS = (TH1D*) f2muSS->Get("hFR" + etaregion);
-   TH1D *h2muSS_lochi2 = (TH1D*) f2muSS_lochi2->Get("hFR" + etaregion);
-   TH1D *h2muSS_hichi2 = (TH1D*) f2muSS_hichi2->Get("hFR" + etaregion);
-   TH1D *hQCD1mu = (TH1D*) f1mu->Get("hFR_QCD" + etaregion);
-   TH1D *hQCD2muSS = (TH1D*) f2muSS->Get("hFR_QCD" + etaregion);
-   TH1D *hQCD2muSS_lochi2 = (TH1D*) f2muSS_lochi2->Get("hFR_QCD" + etaregion);
-   TH1D *hQCD2muSS_hichi2 = (TH1D*) f2muSS_hichi2->Get("hFR_QCD" + etaregion);
+   // new FR
+   TFile *f20190918_ptdiff = TFile::Open("/afs/cern.ch/work/e/echapon/public/DY_pA_2016/FR_HCK_v20190918/fakerate_QCDopt3_ptdif_correctedFR.root");
+   TFile *f20190918_ptincl = TFile::Open("/afs/cern.ch/work/e/echapon/public/DY_pA_2016/FR_HCK_v20190918/fakerate_QCDopt3_ptincl_correctedFR.root");
+
+   TH1D *h1mu = (TH1D*) f1mu->Get("hFR2" + etaregion);
+   TH1D *h2muSS = (TH1D*) f2muSS->Get("hFR2" + etaregion);
+   TH1D *h2muSS_lochi2 = (TH1D*) f2muSS_lochi2->Get("hFR2" + etaregion);
+   TH1D *h2muSS_hichi2 = (TH1D*) f2muSS_hichi2->Get("hFR2" + etaregion);
+   TH1D *hQCD1mu = (TH1D*) f1mu->Get("hFR2_QCD" + etaregion);
+   TH1D *hQCD2muSS = (TH1D*) f2muSS->Get("hFR2_QCD" + etaregion);
+   TH1D *hQCD2muSS_lochi2 = (TH1D*) f2muSS_lochi2->Get("hFR2_QCD" + etaregion);
+   TH1D *hQCD2muSS_hichi2 = (TH1D*) f2muSS_hichi2->Get("hFR2_QCD" + etaregion);
+
+   TH1D *hFR20190918_ptdiff = (TH1D*) f20190918_ptdiff->Get("FR_template" + etaregion);
+   TH1D *hFR20190918_ptincl = (TH1D*) f20190918_ptincl->Get("FR_template" + etaregion);
+
 
    TH1D *htemplate = (TH1D*) ftemplate->Get("FR_template" + etaregion);
 
@@ -22,6 +33,14 @@ void compareFR(TString etaregion) { // "", "_barrel", "_endcap"
    if (htemplate) {
       htemplate->SetMarkerSize(1);
       htemplate->SetMarkerStyle(20);
+      hFR20190918_ptdiff->SetMarkerSize(1);
+      hFR20190918_ptdiff->SetMarkerStyle(20);
+      hFR20190918_ptdiff->SetMarkerColor(kGray);
+      hFR20190918_ptdiff->SetLineColor(kGray);
+      hFR20190918_ptincl->SetMarkerSize(1);
+      hFR20190918_ptincl->SetMarkerStyle(20);
+      hFR20190918_ptincl->SetMarkerColor(kGray+2);
+      hFR20190918_ptincl->SetLineColor(kGray+2);
    }
 
    h1mu->SetMarkerStyle(21);
@@ -52,14 +71,19 @@ void compareFR(TString etaregion) { // "", "_barrel", "_endcap"
 
    TCanvas c1;
    c1.SetLogx();
-   h1mu->Draw();
-   h1mu->GetYaxis()->SetRangeUser(0,1);
+   h2muSS->Draw();
+   h2muSS->GetYaxis()->SetRangeUser(0,1);
+   h2muSS->GetXaxis()->SetTitle("muon p_{T} [GeV]");
    c1.Update();
 
-   hQCD1mu->Draw("same");
-   h2muSS->Draw("same");
+   // hQCD1mu->Draw("same");
+   // h1mu->Draw("same");
    hQCD2muSS->Draw("same");
-   if (htemplate) htemplate->Draw("same");
+   if (htemplate) {
+      htemplate->Draw("same");
+      // hFR20190918_ptdiff->Draw("same");
+      // hFR20190918_ptincl->Draw("same");
+   }
 
    TLegend *tleg = new TLegend(0.2,0.65,0.5,0.95);
    tleg->SetBorderSize(0);
@@ -67,9 +91,11 @@ void compareFR(TString etaregion) { // "", "_barrel", "_endcap"
    else if (etaregion=="_endcap") tleg->SetHeader("|#eta|>1.2");
    else tleg->SetHeader("|#eta|<2.4");
    tleg->AddEntry(htemplate, "Data, template", "lp");
-   tleg->AddEntry(h1mu, "Data-MC, 1#mu", "lp");
+   // tleg->AddEntry(hFR20190918_ptdiff, "Data, FR20190918_ptdiff", "lp");
+   // tleg->AddEntry(hFR20190918_ptincl, "Data, FR20190918_ptincl", "lp");
+   // tleg->AddEntry(h1mu, "Data-MC, 1#mu", "lp");
    tleg->AddEntry(h2muSS, "Data-MC, 2#mu SS", "lp");
-   tleg->AddEntry(hQCD1mu, "QCD MC, 1#mu", "lp");
+   // tleg->AddEntry(hQCD1mu, "QCD MC, 1#mu", "lp");
    tleg->AddEntry(hQCD2muSS, "QCD MC, 2#mu SS", "lp");
    tleg->Draw();
    c1.SaveAs("compareFR" + etaregion + ".pdf");
