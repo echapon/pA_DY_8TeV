29a2c3922013a2dfeffbbdf4c2f47b2670c15cea
Emilien Chapon <emilien.chapon@cern.ch>
2019-09-23 11:06:00 +0200

    Update script for making systs

--- DYAnalysis/makeXsecs.sh
+++ DYAnalysis/makeXsecs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # nominal
-root -l  -b -q ControlPlots/DrawControlPlot.C+'("None",kTRUE,"Lumi","MomCorr00","rewboth",true)'
+root -l  -b -q ControlPlots/DrawControlPlot.C+'("PAL3Mu12",kTRUE,"Lumi","MomCorr00","rewboth",true)'
 root -l -b -q Plots/myXsec.C+'("ROOTFile_YieldHistogram_MomCorr00_rewboth_tnprew.root","ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_rewboth.root","Plots/results/xsec_nom.root",false)'
 
 # no muon momentum correction
-root -l  -b -q ControlPlots/DrawControlPlot.C+'("None",kTRUE,"Lumi","MomUnCorr","rewboth",true)'
+root -l  -b -q ControlPlots/DrawControlPlot.C+'("PAL3Mu12",kTRUE,"Lumi","MomUnCorr","rewboth",true)'
 root -l -b -q Plots/myXsec.C+'("ROOTFile_YieldHistogram_MomUnCorr_rewboth_tnprew.root","ROOTFile_Histogram_Acc_Eff_MomUnCorr_Powheg_PAL3Mu12_0_rewboth.root","Plots/results/xsec_MomUnCorr.root",true)'
 
 # Ntrk rewt
-root -l  -b -q ControlPlots/DrawControlPlot.C+'("None",kTRUE,"Lumi","MomCorr00","rewNtracks",true)'
+root -l  -b -q ControlPlots/DrawControlPlot.C+'("PAL3Mu12",kTRUE,"Lumi","MomCorr00","rewNtracks",true)'
 root -l -b -q Plots/myXsec.C+'("ROOTFile_YieldHistogram_MomCorr00_rewNtracks_tnprew.root","ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_rewNtracks.root","Plots/results/xsec_rewNtracks.root",false)'
 
 # no tnp rew in histo filling
-root -l  -b -q ControlPlots/DrawControlPlot.C+'("None",kTRUE,"Lumi","MomCorr00","rewboth",false)'
+root -l  -b -q ControlPlots/DrawControlPlot.C+'("PAL3Mu12",kTRUE,"Lumi","MomCorr00","rewboth",false)'
 root -l -b -q Plots/myXsec.C+'("ROOTFile_YieldHistogram_MomCorr00_rewboth_notnprew.root","ROOTFile_Histogram_Acc_Eff_MomCorr00_Powheg_PAL3Mu12_0_rewboth.root","Plots/results/xsec_notnprew.root",false)'
 
 # # part 1
