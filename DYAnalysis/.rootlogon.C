4d1ec6dd49679bfe90e74b77de1692fe4bcf0412
Emilien Chapon <emilien.chapon@cern.ch>
2019-09-23 11:02:37 +0200

    Working setup for LHAPDF on lxplus

--- DYAnalysis/.rootlogon.C
+++ DYAnalysis/.rootlogon.C
@@ -16,4 +16,10 @@
    // CVMFS version (not working with ROOT <= 6.06, which is all I could find on lxplus...)
    // gSystem->SetIncludePath("-I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/lhapdf/6.1.6-ikhhed/include/");
    // gSystem->AddLinkedLibs("/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/lhapdf/6.1.6-ikhhed/lib/libLHAPDF.so");
+
+   // setup LHAPDF for lxplus
+   if (TString(gSystem->Getenv("HOSTNAME")).Contains("lxplus")) {
+      gROOT->ProcessLine(".include /afs/cern.ch/work/e/echapon/private/xFitter/deps/lhapdf/include/");
+      gSystem->Load("/afs/cern.ch/work/e/echapon/private/xFitter/deps/lhapdf/lib/libLHAPDF.so");
+   }
 }
