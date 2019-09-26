fa1dd2e84a1da4912482522aa27df1bb9eb4f576
HyunchulKim <hyunchul.kim@cern.ch>
2019-09-26 10:13:03 +0200

    update codes for FR

--- DYAnalysis/BkgEst/interface/cuts.h
+++ DYAnalysis/BkgEst/interface/cuts.h
@@ -3,7 +3,8 @@
 
 namespace cuts {
    double ptmin1 = 15;
-   double ptmin2 = 10; // 10
+   double ptmin2 = 10; // 10(master : 10, for FR : 7)
+	double ptmin3 = 10;
    double etamax = 2.4;
    double isomax = 0.2;//0.15;
    const char* trig = "HLT_PAL3Mu12_v1";
