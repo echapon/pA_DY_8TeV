c395a8cbd869ab0a8b98d99906601da57b803316
Emilien Chapon <emilien.chapon@cern.ch>
2019-10-15 13:09:25 +0200

    Update closure test for FSR

--- DYAnalysis/FSRCorrection/DrawClosureTest_FSR.C
+++ DYAnalysis/FSRCorrection/DrawClosureTest_FSR.C
@@ -1,145 +1,90 @@
-#include <TChain.h>
-#include <TFile.h>
-#include <TH1D.h>
-#include <TH2D.h>
-#include <TLorentzVector.h>
-#include <TStopwatch.h>
-#include <TTimeStamp.h>
-#include <TString.h>
-#include <TLegend.h>
-#include <THStack.h>
-#include <TPad.h>
-#include <TCanvas.h>
-#include <TColor.h>
-#include <TAttMarker.h>
-#include <TF1.h>
-#include <TStyle.h>
-#include <TEfficiency.h>
-#include <stdio.h>
-// #include <TGraphAsymmErrors.h>
-#include <vector>
-#include <TROOT.h>
-#include <TLatex.h>
+#include "TFile.h"
+#include "../Include/MyCanvas.C"
+#include "../BkgEst/interface/defs.h"
+#include "../Include/CMS_lumi.C"
+#include "../Include/PlotTools.h"
+#include "../Include/UnfoldUtils.h"
 
-#include "Include/MyCanvas.C"
-#include "Include/UnfoldUtils.h"
-
+using namespace DYana;
 using unfold::gUnfold;
 
 void CalculateFractionPerBin(TH2D *h_nEvents, TH1* h_Truth, TH2D *h_Response);
 TH2D* Transpose( TH2D* h_2D );
 void MakeCanvas_RespM( TH2D *h_RespM, TString var, int run );
+void DrawClosureTest_FSR(TFile *f, var thevar);
 
-void DrawClosureTest_FSR(TString var="mass", // what variable? mass, pt, rap1560, rap60120
-      TString sample="Original", // which distribution to unfold? Powheg_0, Powheg_1, Powheg_2, Pyquen_0, Original (=same as used for response matrix)
-      int run=0) // 0=all, 1=pPb, 2=PbP
-{
-	gROOT->SetBatch( kTRUE );
+void DrawClosureTest_FSR(const char* matrixfile="FSRCorrection/ROOTFile_FSRCorrections_DressedLepton_Powheg_0.root") {
+   TFile *f = TFile::Open(matrixfile);
+   for (int i=0; i<var::ALLvar; i++) {
+      var thevar_i = static_cast<var>(i);
+      DrawClosureTest_FSR(f, thevar_i);
+   }
+}
+void DrawClosureTest_FSR(TFile *f, var thevar) {
+	setTDRStyle();
+	gROOT->SetStyle( "tdrStyle" );
 
-	TString FileLocation = "./";
-	TFile *f_respM = new TFile(FileLocation + Form("/ROOTFile_FSRCorrections_DressedLepton_Powheg_%d.root",run));
-	TFile *f_dist;
-   if (sample=="Original") f_dist = f_respM;
-   else if (sample!="Data") f_dist = new TFile(FileLocation + Form("/ROOTFile_FSRCorrections_DressedLepton_%s.root",sample.Data()));
-   else f_dist = new TFile(FileLocation + "ROOTFile_YieldHistogram.root");
+   // define the bins
+   const char* thevarname = varname(thevar);
+   TString var(thevarname);
+   int nbinsv = nbinsvar(thevar);
+   double* binsv = binsvar(thevar);
 
-   TString label;
-   if (var=="mass") label = "Mass [GeV]";
-   else if (var=="pt") label = "p_{T} [GeV]";
-   else if (var.Contains("rap")) label = "y";
-   TString label2 = (sample=="Data") ? "Reconstructed " : "Gen-level ";
+
 
 	///////////////////////////
 	// -- Response Matrix -- //
 	///////////////////////////
-	TH1D *h_Truth_TUnfold = (TH1D*)f_respM->Get("h_" + var + "_preFSR");
-	TH2D* h_nEvents = (TH2D*)f_respM->Get("h_" + var + "_postpreFSR_tot");
+	TH1D *h_Truth_TUnfold = (TH1D*)f->Get("h_" + var + "_preFSR");
+   TH1D *h_Measured_TUnfold = (TH1D*) f->Get(Form("h_%s_postFSR",thevarname));
+	TH2D* h_nEvents = (TH2D*)f->Get("h_" + var + "_postpreFSR_tot");
 	h_nEvents->SetName("h_nEvents");
 	TH2D* h_RespM = (TH2D*)h_nEvents->Clone("h_RespM");
-	CalculateFractionPerBin(h_nEvents, h_Truth_TUnfold, h_RespM);
-	MakeCanvas_RespM( h_RespM, var, run );
+   // CalculateFractionPerBin(h_nEvents, h_Truth_TUnfold, h_RespM);
+   // MakeCanvas_RespM( h_RespM, var, run );
 
 	////////////////////////
 	// -- Closure Test -- //
-	////////////////////////
-   // init the TUnfold object
-   // NB: no regularisation except for mass
-   unfold::initTUnfold(h_nEvents,TUnfold::kHistMapOutputVert, var=="mass" ? TUnfold::kRegModeCurvature : TUnfold::kRegModeNone); 
+   ////////////////////////
+   TH2D *histEmatTotal = NULL;
+   TH1D *h_unfolded = unfold::unfold_MLE(h_Measured_TUnfold,h_RespM,histEmatTotal);
 
-   // get the histos
-   TH1D *h_truth = (TH1D*) f_dist->Get("h_" + var + "_preFSR");
-   TH1D *h_postFSR = (TH1D*) f_dist->Get("h_" + var + "_postFSR");
-   if (sample=="Data") {
-      h_postFSR = (TH1D*) f_dist->Get("h_yield_OS_MCBasedBkg1");
+	MyCanvas *myc = new MyCanvas(Form("FSRCorrection/c_ClosureTest_%s",thevarname), xaxistitle(thevar), "Number of events");
+	if (thevar != rap60120 && thevar != rap1560) {
+      myc->SetLogx();
+      fixXaxis(h_Measured_TUnfold);
+      fixXaxis(h_unfolded);
+      fixXaxis(h_Truth_TUnfold);
    }
-   if(gUnfold->SetInput(h_postFSR)>=10000) {
-      std::cout<<"Unfolding result may be wrong\n";
-   }
-
-   // do the unfolding
-   unfold::doUnfold();
-
-   // get the unfolded distribution, with regularisation
-   TH1D* h_unfolded = (TH1D*) gUnfold->GetOutput("h_unfolded");
-   // also without regularisation
-   gUnfold->DoUnfold(0);
-   TH1D* h_unfolded_mle = (TH1D*) gUnfold->GetOutput("h_unfolded_mle");
-
-	MyCanvas *myc = new MyCanvas(Form("c_ClosureTest_%s_%s_%d",var.Data(),sample.Data(),run), label2+label, "Number of events");
-	if (!var.Contains("rap")) myc->SetLogx();
 	myc->SetLogy(0);
-	if (var=="mass") myc->SetRatioRange(0.7, 1.3);
-   else myc->SetRatioRange(0.91, 1.09);
-	if (sample!="Data")
-      myc->CanvasWithThreeHistogramsRatioPlot( h_postFSR, h_unfolded, h_truth, 
-											 "post-FSR", "Unfolded", "pre-FSR", "Ratio to pre-FSR",
+	myc->SetRatioRange(0.7, 1.3);
+	myc->CanvasWithThreeHistogramsRatioPlot( h_Measured_TUnfold, h_unfolded, h_Truth_TUnfold, 
+											 "Post-FSR", "Unfolded", "Pre-FSR", "Ratio to Pre-FSR",
 											 kBlue, kGreen+1, kRed);
-   else
-      myc->CanvasWithHistogramsRatioPlot(h_postFSR, h_unfolded, 
-            "Data post FSR", "Data unfolded", "Ratio to pre-FSR",
-            kBlue, kGreen+1);
 	myc->PrintCanvas();
 
-	MyCanvas *myc_mle = new MyCanvas(Form("c_ClosureTest_mle_%s_%s_%d",var.Data(),sample.Data(),run), label2+label, "Number of events");
-	if (!var.Contains("rap")) myc_mle->SetLogx();
-	myc_mle->SetLogy(0);
-	if (var=="mass") myc_mle->SetRatioRange(0.7, 1.3);
-   else myc_mle->SetRatioRange(0.91, 1.09);
-   if (sample != "Data") 
-      myc_mle->CanvasWithThreeHistogramsRatioPlot( h_postFSR, h_unfolded_mle, h_truth, 
-											 "post-FSR", "Unfolded (MLE)", "pre-FSR", "Ratio to pre-FSR",
-											 kBlue, kGreen+1, kRed);
-   else
-      myc_mle->CanvasWithHistogramsRatioPlot(h_postFSR, h_unfolded_mle, 
-            "Data post FSR", "Data unfolded (MLE)", "Ratio to pre-FSR",
-            kBlue, kGreen+1);
-	myc_mle->PrintCanvas();
-
-   // -- Check the detail number -- //
-   if (sample!="Data") {
-      TH1D *h_ratio = (TH1D*)(myc->h_ratio2)->Clone();
-      Int_t nBin = h_ratio->GetNbinsX();
-      Bool_t isPassTest = kTRUE;
-      for(Int_t i=0; i<nBin; i++)
-      {
-         Int_t i_bin = i+1;
-         Double_t ratio = h_ratio->GetBinContent(i_bin);
-         if( (ratio-1) > h_ratio->GetBinError(i_bin) )
-         {
-            Double_t lowerEdge = h_ratio->GetBinLowEdge(i_bin);
-            Double_t upperEdge = h_ratio->GetBinLowEdge(i_bin+1);
-            printf("[%2dth bin] (lowerEdge, upperEdge) = (%.3lf, %.3lf) (Unfolded/Truth) = %.6lf\n", i_bin, lowerEdge, upperEdge, ratio);
-            isPassTest = kFALSE;
-         }
-      }
-      cout << "==============================================" << endl;
-      if( isPassTest ) 
-         cout << "[Pass the closure test]" << endl;
-      else
-         cout << "[Closure test is FAILED! ... Check the details!]" << endl;
-      cout << "==============================================" << endl;
-   }
-
+	// -- Check the detail number -- //
+	TH1D *h_ratio = (TH1D*)(myc->h_ratio2)->Clone();
+	Int_t nBin = h_ratio->GetNbinsX();
+	Bool_t isPassTest = kTRUE;
+	for(Int_t i=0; i<nBin; i++)
+	{
+		Int_t i_bin = i+1;
+		Double_t ratio = h_ratio->GetBinContent(i_bin);
+		if( (ratio-1) > 1e-5 )
+		{
+			Double_t lowerEdge = h_ratio->GetBinLowEdge(i_bin);
+			Double_t upperEdge = h_ratio->GetBinLowEdge(i_bin+1);
+			printf("[%2dth bin] (lowerEdge, upperEdge) = (%.3lf, %.3lf) (Unfolded/Truth) = %.6lf\n", i_bin, lowerEdge, upperEdge, ratio);
+			isPassTest = kFALSE;
+		}
+	}
+	cout << "==============================================" << endl;
+	if( isPassTest ) 
+		cout << "[Pass the closure test]" << endl;
+	else
+		cout << "[Closure test is FAILED! ... Check the details!]" << endl;
+	cout << "==============================================" << endl;
 }
 
 void CalculateFractionPerBin(TH2D *h_nEvents, TH1* h_Truth, TH2D *h_Response)
