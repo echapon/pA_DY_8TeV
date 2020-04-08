bc36dc42141e3f1f0a5be2a1de149fc6d6e9be5f
Emilien Chapon <emilien.chapon@cern.ch>
2020-04-08 19:23:41 +0200

    update

--- DYAnalysis/AccEff/compareMCFM.C
+++ DYAnalysis/AccEff/compareMCFM.C
@@ -5,6 +5,7 @@ void compareMCFM() {
    TFile *fPowheg = TFile::Open("/afs/cern.ch/user/e/echapon/workspace/private/2016_pPb/DY/tree_ana/PADrellYan8TeV/DYAnalysis/ROOTFile_Histogram_Acc_Eff_weights_MomCorr00_Powheg_PAL3Mu12_0_rewboth.root");
    TFile *fMCFM0 = TFile::Open("/afs/cern.ch/user/e/echapon/workspace/private/MCFM/MCFM-8.0/Bin/DYpA8TeV/nominal_1560/Z_only_nlo_CT14nlo_1.00_1.00_DY15600_full_NLO_muM34_EPPS.DA.root");
    TFile *fMCFM1 = TFile::Open("/afs/cern.ch/user/e/echapon/workspace/private/MCFM/MCFM-8.0/Bin/DYpA8TeV/nominal_1560/Z_only_nlo_CT14nlo_90___90___DY15600_full_NLO_mu90_EPPS.DAT.root");
+   TFile *fMCFM2 = TFile::Open("/afs/cern.ch/user/e/echapon/workspace/private/MCFM/MCFM-8.0/Bin/DYpA8TeV/nominal_1560/Z_only_nlo_CT14nlo_1.00_1.00_DY15600_full_NLO_muM34_nCTEQ.D.root");
 
    const int nhists = 12;
    const char* histnamesPowheg[nhists] = {
@@ -55,16 +56,21 @@ void compareMCFM() {
       TH1D *hPowheg = (TH1D*) fPowheg->Get(histnamesPowheg[i]);
       TH1D *hMCFM0 = (TH1D*) fMCFM0->Get(histnamesMCFM[i]);
       TH1D *hMCFM1 = (TH1D*) fMCFM1->Get(histnamesMCFM[i]);
+      TH1D *hMCFM2 = (TH1D*) fMCFM2->Get(histnamesMCFM[i]);
 
       // factor 208 in between MCFM and Powheg
       hMCFM0->Scale(hMCFM0->GetBinWidth(1)*208./1000.);
       hMCFM1->Scale(hMCFM1->GetBinWidth(1)*208./1000.);
+      hMCFM2->Scale(hMCFM1->GetBinWidth(1)*208./1000.);
       // POWHEG is a number of events: divide by lumi
       hPowheg->Scale(1./lumi_all);
 
       MyCanvas c(histnamesMCFM[i],xtitles[i],"Xsec [pb] / bin",800,600);
-      c.CanvasWithThreeHistogramsRatioPlot(hMCFM0,hMCFM1,hPowheg,
-            "MCFM (auto #mu)", "MCFM (#mu=90 GeV)", "Powheg", "Powheg/MCFM");
+      c.SetRatioRange(0.79,1.21);
+      // c.CanvasWithThreeHistogramsRatioPlot(hMCFM0,hMCFM1,hPowheg,
+            // "MCFM (auto #mu)", "MCFM (#mu=90 GeV)", "Powheg", "Powheg/MCFM");
+      c.CanvasWithThreeHistogramsRatioPlot(hMCFM0,hMCFM2,hPowheg,
+            "MCFM (EPPS16)", "MCFM (nCTEQ15)", "Powheg", "Powheg/MCFM");
       c.PrintCanvas();
    }
 }
